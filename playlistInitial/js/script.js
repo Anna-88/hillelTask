0e6cf7cc8ed87c9ab1f60f991f26d9712bd74d77
Ann Sidorova <ann.vit.sidorova@gmail.com>
2021-08-06 00:00:01 +0300

    commit

--- playlistInitial/js/script.js
+++ playlistInitial/js/script.js
@@ -8,5 +8,62 @@ let songs = [{
 }];
 
 window.addEventListener('load', function() {
-	// your cade should be started here
+	 songs.forEach((song) => listSong(song));
+   
+
 });
+
+let listSongs = document.body.querySelector('.songs');
+
+
+
+function listSong(song) {
+  let li = document.createElement('li');
+  li.innerHTML = song.name;
+  listSongs.prepend(li);
+  creatButton('Dislike','delete',li);
+  creatButton('Like','like', li);
+  let inputLike = document.getElementsByClassName('button like');
+  console.log(inputLike);
+  inputLike.onclick = function(event){
+    if(song.isLiked === true){
+      event.target.style.backgroundImage = "./images/img.svg";
+    }
+  }
+}
+
+function creatButton (text,cl,parent) {
+  let input = document.createElement('input');
+  input.value = text;
+  input.className = 'button '  + cl;
+  parent.append(input);
+};
+
+let search = document.querySelector('.input-box');
+let add = document.querySelector('.button-add');
+
+add.addEventListener("click", function () {
+  let song = search.value;
+  let linew = document.createElement('li');
+  linew.innerHTML = song;
+  listSongs.append(linew);
+  creatButton('Dislike','delete',linew);
+  creatButton('Like','like', linew);
+});
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
