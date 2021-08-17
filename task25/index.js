1813fe1bdda1b751e34890855b1c61521d506370
Ann Sidorova <ann.vit.sidorova@gmail.com>
2021-08-17 16:20:23 +0300

    task1

--- task25/index.js
+++ task25/index.js
@@ -11,27 +11,10 @@ let promise = new Promise(function(resolve, reject){
     },2000)
 });
 
-promise.then(response => {
-    console.log(response);
+promise.then(function(resolve){
+  console.log(resolve)
 })
 .catch(error => {
     console.error(error);
 });
 
-
-let p = new Promise(function(resolve, reject){
-    let num = Math.floor(Math.random() * 6) + 1;
-    setTimeout(function(){
-     if(num === 1){
-        resolve("Stay here")
-        return num;
-    }else if(num >= 2){
-        resolve(`Go ${num} steps`)
-    }else{
-        reject('exit');
-    }
-    },2000);
-    
-});
-
-p.then(resolve => console.log(resolve)).catch(reject => console.error(reject));
