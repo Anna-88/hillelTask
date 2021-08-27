fdae2e109c4f44edc94e9fb022c5f8e0227a9a17
Ann Sidorova <ann.vit.sidorova@gmail.com>
2021-08-27 23:37:26 +0300

    test1

---
+++ webpack-project-master/src/js/person.js
@@ -0,0 +1,9 @@
+export default class  Person {
+    constructor (fistName, lastName) {
+        this.fistName = fistName;
+        this.lastName = lastName;
+    }
+    getFullName(){
+        return this.firstName + ' ' + this.lastName;
+    }
+}
